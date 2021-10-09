4ed32003d5a2645f2eded85f8c30a0eb62652285
michaelgranado <87629975+michaelgranado@users.noreply.github.com>
2021-10-08 22:35:44 -0700

    Initial Commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,43 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import com.codepath.apps.restclienttemplate.TimeFormatter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tweet {
+
+    public String body;
+    public String createdAt;
+    public String retweet_count;
+    public String favorite_count;
+    public long id;
+    public User user;
+
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.createdAt = getFormattedTimestamp(jsonObject.getString("created_at"));
+        tweet.id = jsonObject.getLong("id");
+        tweet.retweet_count = jsonObject.getString("retweet_count");
+        tweet.favorite_count = jsonObject.getString("favorite_count");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++){
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return tweets;
+    }
+
+    public static String getFormattedTimestamp(String createdAt){
+        return TimeFormatter.getTimeDifference(createdAt) + " ago";
+    }
+}
